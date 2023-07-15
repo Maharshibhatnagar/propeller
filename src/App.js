dffb8c8e86debb47a192032a69d7fd673f8bdfe0
Maharshibhatnagar <bhatnagarmaharshi@gmail.com>
2023-07-15 21:02:56 +0530

    changed logo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Propeller
+          Learn  About Propeller
         </a>
       </header>
     </div>
